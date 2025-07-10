2adf4f3f695facb9c95fc90d6240e8c81e9fb6dd
SaraEOlivera <saraeolivera@gmail.com>
2025-07-10 10:54:55 -0300

    Ultimos arreglos

--- conversor/app.js
+++ conversor/app.js
@@ -1,11 +1,23 @@
 const primerNumero = document.getElementById("inputUsuario");
 const segundoNumero = document.getElementById("equivalente");
+const textos = document.querySelectorAll('.texto');
+const pFahrenheit = document.getElementById("pFahrenheit");
+const pCelsius = document.getElementById("pCelsius");
+
+textos.forEach(p => {  //ocultar textos
+    p.style.display = 'none';
+})
 
 primerNumero.oninput = () => {
+
+    textos.forEach(p => { //repite p/no mostrar los 2
+        p.style.display = 'none';
+    })
+
     let seleccionado = document.querySelector('input[name="medida"]:checked').value;
     let valor1 = parseFloat(primerNumero.value);
 
-    if(isNaN(valor1)){
+    if(isNaN(valor1)){ 
         segundoNumero.value = '';
         return
     }
@@ -14,13 +26,15 @@ primerNumero.oninput = () => {
 
     if (seleccionado === "Celsius") {
         resultado = (valor1 * 9 / 5) + 32;
+        pFahrenheit.style.display = "block"; //muestra 1 p
         console.log("resultado " + resultado + " celsius")
     }
     else if (seleccionado === "Fahrenheit") {
         resultado = (valor1 - 32) * 5 / 9;
+        pCelsius.style.display = "block"; // muestra el otro
         console.log("resultado " + resultado + " Fahrenheit")
     }
-    segundoNumero.value = resultado.toFixed(2);
+    segundoNumero.value = resultado.toFixed(2); 
 
 }
 
