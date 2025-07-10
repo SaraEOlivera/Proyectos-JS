54169810eaad56ac6d5798bbce14f136dadf2e3e
SaraEOlivera <saraeolivera@gmail.com>
2025-07-10 06:39:56 -0300

    detectar valores del radio button

---
+++ conversor/app.js
@@ -0,0 +1,19 @@
+
+
+document.querySelectorAll('input[name = "medida"]').forEach(opcion => {
+    opcion.addEventListener('change', detectarRadioBtn);
+});
+
+
+function detectarRadioBtn() {
+    let seleccionado = document.querySelector('input[name="medida"]:checked').value;
+    
+    if(seleccionado === "Celsius"){
+        console.log("Elegiste " + seleccionado);
+    }else if (seleccionado === "Fahrenheit"){
+        console.log("Elegiste " + seleccionado);
+    }
+    else {
+        console.log("No Elegiste");
+    }
+}
