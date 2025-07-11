d6d1b5d1ad23c5b0c11e9e68b44e5899f467ed19
SaraEOlivera <saraeolivera@gmail.com>
2025-07-11 06:58:20 -0300

    fin del proyecto

--- conversor/app.js
+++ conversor/app.js
@@ -8,35 +8,50 @@ textos.forEach(p => {  //ocultar textos
     p.style.display = 'none';
 })
 
-primerNumero.oninput = () => {
+function calcularTemperatura() {
 
     textos.forEach(p => { //repite p/no mostrar los 2
         p.style.display = 'none';
     })
 
-    let seleccionado = document.querySelector('input[name="medida"]:checked').value;
+    let seleccionado = document.querySelector('input[name="medida"]:checked');
+
+    if(!seleccionado){
+        alert("No elegiste un tipo de temperatura");
+        return;
+    }
+
     let valor1 = parseFloat(primerNumero.value);
 
-    if(isNaN(valor1)){ 
+    if(isNaN(valor1)){
         segundoNumero.value = '';
-        return
+        return;
     }
 
-    let resultado;
+    let resultado;   
 
-    if (seleccionado === "Celsius") {
+    if (seleccionado.value === "Celsius") {
         resultado = (valor1 * 9 / 5) + 32;
         pFahrenheit.style.display = "block"; //muestra 1 p
-        console.log("resultado " + resultado + " celsius")
+        console.log("resultado " + resultado + " celsius");
     }
-    else if (seleccionado === "Fahrenheit") {
+    else if (seleccionado.value === "Fahrenheit") {
         resultado = (valor1 - 32) * 5 / 9;
         pCelsius.style.display = "block"; // muestra el otro
-        console.log("resultado " + resultado + " Fahrenheit")
+        console.log("resultado " + resultado + " Fahrenheit");
     }
     segundoNumero.value = resultado.toFixed(2); 
-
 }
+
+//llamar a calcular cuando el usuario escribe
+primerNumero.addEventListener('input', calcularTemperatura);
+
+//llamar a calcular cuando se cambia el radio button
+document.querySelectorAll('input[name= "medida"]').forEach(opcion => {
+    opcion.addEventListener('change', calcularTemperatura);
+});
+
+
 
 
         
