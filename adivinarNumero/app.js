7a59a68a4b4c01442c1a5170d28771067614bb47
SaraEOlivera <saraeolivera@gmail.com>
2025-07-14 21:49:22 -0300

    funcion para comparar

--- adivinarNumero/app.js
+++ adivinarNumero/app.js
@@ -1,4 +1,27 @@
+
+let numUsuario = document.getElementById("numUsuario");
+let respuesta = document.getElementById("respuesta");
 let aleatorio = Math.random();
 aleatorio = aleatorio * 100 + 1; 
 aleatorio = Math.trunc(aleatorio);
 console.log(aleatorio);
+
+respuesta.style.display = "none";
+
+function confirmarNumero() {
+    let valorUsuario = parseInt(numUsuario.value);
+    if (valorUsuario > aleatorio){    
+        respuesta.innerText = `${valorUsuario} es muy alto`;
+        respuesta.style.display = "block";
+    }
+    else if (valorUsuario < aleatorio){
+        respuesta.innerText = `${valorUsuario} es muy bajo`;
+        respuesta.style.display = "block";
+    }
+    else if (valorUsuario === aleatorio){
+        respuesta.innerText = `Adivinaste! El número era ${aleatorio}`;
+        respuesta.style.display = "block";
+    }
+}
+let btnVerificar = document.getElementById("verificarNumero");
+btnVerificar.addEventListener("click", confirmarNumero);
