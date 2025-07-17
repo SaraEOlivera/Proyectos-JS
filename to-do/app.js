7a6e335a6f2d908212cb76bbf000494e37cbeffa
SaraEOlivera <saraeolivera@gmail.com>
2025-07-16 21:56:13 -0300

    Funcion para agregar tareas del input

--- to-do/app.js
+++ to-do/app.js
@@ -1,2 +1,18 @@
 
+let lista = document.getElementById('lista');
+let nuevaTarea = document.getElementById('nuevaTarea');
+nuevaTarea.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        let nuevoLi = document.createElement('li');
+        let nuevaLinea = document.createElement('hr');
 
+        nuevoLi.classList.add('elementoLista');
+        nuevaLinea.classList.add('elementoLinea');
+
+        nuevoLi.innerText = nuevaTarea.value;
+
+        document.querySelector('.listaTareas').appendChild(nuevoLi);
+        document.querySelector('.listaTareas').appendChild(nuevaLinea);
+        nuevaTarea.value = '';   
+    }
+})
