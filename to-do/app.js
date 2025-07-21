e1c5ad5dc7666c2aaa8e94466ed5186a87cc9c34
SaraEOlivera <saraeolivera@gmail.com>
2025-07-21 11:24:01 -0300

    Iconos para editar y elimitar tarea

--- to-do/app.js
+++ to-do/app.js
@@ -17,17 +17,35 @@ nuevaTarea.addEventListener('keyup', (e) => {
         let divTarea = document.createElement('div');
         divTarea.classList.add('divTarea');
 
+        let divTexto = document.createElement('div');
+        divTexto.classList.add('divtexto');
+
         let spanCirculo = document.createElement('span');
         spanCirculo.classList.add('circulo');
-        
 
         let spanTexto = document.createElement('span');
         spanTexto.classList.add('txtTarea');
         spanTexto.textContent = nuevaTarea.value;
 
-        
-        divTarea.appendChild(spanCirculo);
-        divTarea.appendChild(spanTexto);
+        let divIconos = document.createElement('div');
+        divIconos.classList.add('iconos');
+
+        let iTrash = document.createElement('i');
+        iTrash.classList.add('fa-trash-can');
+        iTrash.classList.add('fa-solid');
+
+        let iEdit = document.createElement('i');
+        iEdit.classList.add('fa-pencil');
+        iEdit.classList.add('fa-solid');
+
+        divTarea.appendChild(divTexto);
+        divTarea.appendChild(divIconos);
+
+        divTexto.appendChild(spanCirculo);
+        divTexto.appendChild(spanTexto);
+
+        divIconos.appendChild(iTrash);
+        divIconos.appendChild(iEdit);
 
         nuevoLi.appendChild(divTarea);
         nuevoLi.appendChild(nuevaLinea);
@@ -35,6 +53,7 @@ nuevaTarea.addEventListener('keyup', (e) => {
         document.querySelector('.listaTareas').appendChild(nuevoLi);
 
         nuevaTarea.value = '';   
+
     };
 });
 
